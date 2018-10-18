9e6ef01fd75266a4c0d8789c44aa4583c6a86adb
mkerb003 <38334956+mkerb003@users.noreply.github.com>
2018-10-18 13:51:03 -0400

    Updates
    Misc

--- SnapShare.py
+++ SnapShare.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import flickrapi
+import tkinter
+from tkinter import filedialog
 
 
 # returns the API or secret key
@@ -13,12 +15,19 @@ def get_key(key_type):
     return flickr_api_key
 
 
-# initializes a flickr object with API key
-def initialize_api():
+# initializes a flickr object with anonymous user
+def initialize_api_anon():
     global flickr
     sec_key = get_key("flickr_secret")
     api_key = get_key("flickr_key")
     flickr = flickrapi.FlickrAPI(api_key, sec_key, format='parsed-json')
+
+
+# initializes a flickr object with logged in user
+def initialize_api_user(user_token):
+    sec_key = get_key("flickr_secret")
+    api_key = get_key("flickr_key")
+    flickr = flickrapi.FlickrAPI(api_key, sec_key, format='parsed-json', token=user_token)
 
 
 # returns info for a specified photo_id
@@ -40,14 +49,29 @@ def overwrite_file(data, file_name):
         json.dump(data, file, indent=4)
 
 
+# generates a user token with read only permissions
 def authenticate_read_only():
     flickr.authenticate_via_browser(perms='read')
 
 
+# generates a user token with write permissions
 def authenticate_write_perms():
     flickr.authenticate_via_browser(perms='write')
 
 
-initialize_api()
-print(flickr.photos.getInfo(photo_id='43593563660'))
-print(photo_info('43593563660'))
+# checks if the user token is valid
+def check_token():
+    return flickr.token_valid()
+
+
+# uploads a photo
+def upload_photo(file_name):
+    return file_name
+
+
+def get_filename():
+    root = Tk()
+    root.filename = filedialog.askopenfilename()
+
+upload_photo(get_filename())
+
